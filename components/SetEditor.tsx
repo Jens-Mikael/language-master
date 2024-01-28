573c93a130168f7232c95372e668ca856073cf17
Jesse <jesse@Jesse-MacBook-Pro.local>
2024-01-28 17:41:42 +0200

    fr

--- components/SetEditor.tsx
+++ components/SetEditor.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import MobileTap from "./MobileTap";
 import SetSettings from "./SetSettings";
+import { Suspense } from "react";
 
 interface IProps {
   uid: string;
@@ -49,114 +50,102 @@ const SetEditor = ({ uid, type }: IProps) => {
   console.log(type);
 
   return (
-    <div className="min-h-full flex justify-center items-center pt-4">
-      <div className="w-full max-w-5xl flex flex-col gap-20">
-        {/* HEADER */}
-        <div className="flex justify-between items-center gap-2">
-          <div className="flex flex-col gap-2">
-            <div className="text-2xl font-bold">
-              {type === "studyDraft" ? "Create a new study set" : "Edit set:"}
+    <Suspense>
+      <div className="min-h-full flex justify-center items-center pt-4">
+        <div className="w-full max-w-5xl flex flex-col gap-20">
+          {/* HEADER */}
+          <div className="flex justify-between items-center gap-2">
+            <div className="flex flex-col gap-2">
+              <div className="text-2xl font-bold">
+                {type === "studyDraft" ? "Create a new study set" : "Edit set:"}
+              </div>
             </div>
-          </div>
-          <MobileTap
-            onClick={() => {
-              if (data?.head.title !== "") {
-                if (type === "studyDraft") {
-                  if (data) submitSet();
-                  router.push("/");
-                } else {
-                  router.push(`/sets/${type}`);
+            <MobileTap
+              onClick={() => {
+                if (data?.head.title !== "") {
+                  if (type === "studyDraft") {
+                    if (data) submitSet();
+                    router.push("/");
+                  } else {
+                    router.push(`/sets/${type}`);
+                  }
                 }
-              }
-            }}
-            className="bg-blue-600 hover:bg-indigo-600 hover:scale-105 transition px-4 sm:px-6 py-3 sm:py-4 rounded-lg text-md sm:text-lg font-medium"
-          >
-            {type === "studyDraft" ? "Create" : "Save"}
-          </MobileTap>
-        </div>
+              }}
+              className="bg-blue-600 hover:bg-indigo-600 hover:scale-105 transition px-4 sm:px-6 py-3 sm:py-4 rounded-lg text-md sm:text-lg font-medium"
+            >
+              {type === "studyDraft" ? "Create" : "Save"}
+            </MobileTap>
+          </div>
 
-        {/* SET DETAILS */}
-        <div className="w-full max-w-[512px] flex flex-col gap-6">
-          <div>
-            <InputField
-              label="TITLE"
-              placeholder="Enter a title..."
-              cardId="editSetTitle"
-              value={data?.head.title!}
-              setId={data?.id!}
-              type="title"
-            />
-          </div>
-          <div>
-            <InputField
-              label="DESCRIPTION"
-              placeholder="Add a description..."
-              cardId="editSetDesc"
-              value={data?.head.description!}
-              setId={data?.id!}
-              type="description"
-            />
-          </div>
-        </div>
-        {type !== "studyDraft" && (
-          <div className="flex justify-end">
-            <SetSettings
-              setId={type}
-              title={data?.head.title!}
-              isPublic={data?.isPublic!}
-            />
-          </div>
-        )}
-
-        {/* CARDS */}
-        <div className="flex flex-col gap-10">
-          {Object.keys(data!.body).map((cardId, i) => (
-            <AnimatePresence mode="popLayout" key={i}>
-              <motion.div
-                key={i}
-                initial={{ opacity: 0.5, scale: 0.5, y: "20%" }}
-                animate={{ opacity: 1, scale: 1, y: 0 }}
-                exit={{ opacity: 0.5, scale: 0.5 }}
-              >
-                <NewStudySetCard
-                  obj={data!.body[cardId]}
-                  cardId={cardId}
-                  index={i}
-                  key={cardId}
-                  setId={data!.id}
-                  type={type}
-                />
-              </motion.div>
-            </AnimatePresence>
-          ))}
-          <MobileTap
-            onClick={() => addStudyCard()}
-            className="group w-full relative bg-white/10 rounded-xl flex items-center justify-center p-10 hover:scale-105 transition cursor-pointer"
-          >
-            <div className="border-b-4 border-blue-500 pb-2 font-bold text-xl group-hover:border-indigo-600 transition w-fit">
-              + New Card
+          {/* SET DETAILS */}
+          <div className="w-full max-w-[512px] flex flex-col gap-6">
+            <div>
+              <InputField
+                label="TITLE"
+                placeholder="Enter a title..."
+                cardId="editSetTitle"
+                value={data?.head.title!}
+                setId={data?.id!}
+                type="title"
+              />
             </div>
-            <div className="absolute left-10 font-bold text-xl">
-              {Object.keys(data!.body).length + 1}
+            <div>
+              <InputField
+                label="DESCRIPTION"
+                placeholder="Add a description..."
+                cardId="editSetDesc"
+                value={data?.head.description!}
+                setId={data?.id!}
+                type="description"
+              />
             </div>
-          </MobileTap>
-
-          {/* {type != "studyDraft" && (
+          </div>
+          {type !== "studyDraft" && (
             <div className="flex justify-end">
-              <MobileTap>
-                <Link
-                  href="/"
-                  onClick={() => deleteSet()}
-                  className="py-3 px-5 rounded-lg border border-white/40 bg-white/5 hover:bg-white/10 hover:scale-105 transition"
-                >
-                  Delete Set
-                </Link>
-              </MobileTap>
+              <SetSettings
+                setId={type}
+                title={data?.head.title!}
+                isPublic={data?.isPublic!}
+              />
             </div>
-          )} */}
+          )}
+
+          {/* CARDS */}
+          <div className="flex flex-col gap-10">
+            {Object.keys(data!.body).map((cardId, i) => (
+              <AnimatePresence mode="popLayout" key={i}>
+                <motion.div
+                  key={i}
+                  initial={{ opacity: 0.5, scale: 0.5, y: "20%" }}
+                  animate={{ opacity: 1, scale: 1, y: 0 }}
+                  exit={{ opacity: 0.5, scale: 0.5 }}
+                >
+                  <NewStudySetCard
+                    obj={data!.body[cardId]}
+                    cardId={cardId}
+                    index={i}
+                    key={cardId}
+                    setId={data!.id}
+                    type={type}
+                  />
+                </motion.div>
+              </AnimatePresence>
+            ))}
+            <MobileTap
+              onClick={() => addStudyCard()}
+              className="group w-full relative bg-white/10 rounded-xl flex items-center justify-center p-10 hover:scale-105 transition cursor-pointer"
+            >
+              <div className="border-b-4 border-blue-500 pb-2 font-bold text-xl group-hover:border-indigo-600 transition w-fit">
+                + New Card
+              </div>
+              <div className="absolute left-10 font-bold text-xl">
+                {Object.keys(data!.body).length + 1}
+              </div>
+            </MobileTap>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
