7d9dc7cea7447ea3be042bae1e4217a09637e64a
Jesse <jesse@Jesse-MacBook-Pro.local>
2024-01-28 17:47:11 +0200

    fr

--- app/search/studySets/page.tsx
+++ app/search/studySets/page.tsx
@@ -22,58 +22,62 @@ const SearchStudySetsPage = () => {
 
   if (isSearchLoading) return <div>loading</div>;
   return (
-    <Suspense>
-      <div>
-        {studySets && studySets.length >= 1 ? (
-          <div className="grid grid-cols-1 gap-5 sm:gap-5 md:grid-cols-2 w-full">
-            {studySets.map((obj) => (
-              <MobileTap
-                key={obj.id}
-                className="group hover:scale-105 transition w-full h-[160px] bg-white/20 rounded-xl text-start truncate relative"
-              >
-                <Link href={`/sets/${obj.id}`}>
-                  <div className="gap-10 flex flex-col justify-between p-5 h-full">
-                    <div className="flex flex-col gap-2">
-                      <div className="font-medium text-xl">{obj.title}</div>
-                      <div className="italic text-sm">{obj.description}</div>
-                    </div>
-
-                    <Link
-                      href={`/users/${obj.creator}/${
-                        obj.creator === currentUser?.uid ? "" : "studySets"
-                      }`}
-                      className="flex gap-3 items-center hover:scale-105 transition w-fit"
-                    >
-                      {!creatorsIsLoading && creatorsData && (
-                        <>
-                          <Image
-                            className="rounded-full"
-                            width={24}
-                            height={24}
-                            src={creatorsData[obj.creator].photoURL}
-                            alt="usr photoURL"
-                          />
-                          <div className="font-medium text-sm">
-                            {creatorsData[obj.creator].displayName}
-                          </div>
-                        </>
-                      )}
-                    </Link>
+    <div>
+      {studySets && studySets.length >= 1 ? (
+        <div className="grid grid-cols-1 gap-5 sm:gap-5 md:grid-cols-2 w-full">
+          {studySets.map((obj) => (
+            <MobileTap
+              key={obj.id}
+              className="group hover:scale-105 transition w-full h-[160px] bg-white/20 rounded-xl text-start truncate relative"
+            >
+              <Link href={`/sets/${obj.id}`}>
+                <div className="gap-10 flex flex-col justify-between p-5 h-full">
+                  <div className="flex flex-col gap-2">
+                    <div className="font-medium text-xl">{obj.title}</div>
+                    <div className="italic text-sm">{obj.description}</div>
                   </div>
-                  <div className="group-hover:h-1 h-0 w-full absolute bottom-0 bg-blue-500 transition-all" />
-                </Link>
-              </MobileTap>
-            ))}
-          </div>
-        ) : (
-          <div className="flex text-lg">
-            No results for "
-            <div className="italic">{searchParams.get("query")}</div>"
-          </div>
-        )}
-      </div>
-    </Suspense>
+
+                  <Link
+                    href={`/users/${obj.creator}/${
+                      obj.creator === currentUser?.uid ? "" : "studySets"
+                    }`}
+                    className="flex gap-3 items-center hover:scale-105 transition w-fit"
+                  >
+                    {!creatorsIsLoading && creatorsData && (
+                      <>
+                        <Image
+                          className="rounded-full"
+                          width={24}
+                          height={24}
+                          src={creatorsData[obj.creator].photoURL}
+                          alt="usr photoURL"
+                        />
+                        <div className="font-medium text-sm">
+                          {creatorsData[obj.creator].displayName}
+                        </div>
+                      </>
+                    )}
+                  </Link>
+                </div>
+                <div className="group-hover:h-1 h-0 w-full absolute bottom-0 bg-blue-500 transition-all" />
+              </Link>
+            </MobileTap>
+          ))}
+        </div>
+      ) : (
+        <div className="flex text-lg">
+          No results for "
+          <div className="italic">{searchParams.get("query")}</div>"
+        </div>
+      )}
+    </div>
   );
 };
 
-export default SearchStudySetsPage;
+const SuspensedSearchStudySetsPage = () => (
+  <Suspense>
+    <SearchStudySetsPage />
+  </Suspense>
+);
+
+export default SuspensedSearchStudySetsPage;
